96777a231ebd17f2244497d3e89c7b977a1a0cb4
Paul Maliakel <paulbalu93@gmail.com>
2020-12-18 17:31:11 +0530

    Get, Put, Post

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -1,56 +1,81 @@
-
 const express = require('express');
-const path = require('path')
+const path = require('path');
 const router = express.Router();
-const {readDB, writeDB} = require('../../utils')
-const uniqid = require('uniqid')
-const {readJSON} = require("fs-extra")
-const productsPath = path.join(__dirname, "products.json")
+const { readDB, writeDB } = require('../../utils');
+const uniqid = require('uniqid');
+const { readJSON } = require('fs-extra');
+const productsPath = path.join(__dirname, 'products.json');
 
-router.route('/').get( async (req,res,next)=> {
-    try {
-       const products = await readDB(productsPath)
-       res.send(products)
-    } catch (error) {
-        console.log(error);
-    }
-    
-    })
-    
-    router.route('/').post(async(req,res,next)=> {
-        try {
-            
-            const products = await readDB(productsPath)
-          const newProduct = {
+router.route('/').get(async (req, res, next) => {
+	try {
+        let products = await readDB(productsPath);
+		// console.log(req.query);
+		// if (req.query) {
 
-            ...req.body, _id: uniqid(),
-          } ;
-          products.push(newProduct) 
-          await writeDB(productsPath, newProduct)
-          res.send(newProduct)
-        console.log(req.body)
-        } catch (error) {
-            console.log(error);
+        // }
+        for (key in req.query)
+        {
+            console.log(req.query[key])
+            products = products.filter((product)=> !product[key] ? product : product[key].toString() === req.query[key]) //toSting()
         }
-        
-        });
-    
-        router.route('/:id').put((req,res,next)=> {
-            try {
-                
-            } catch (error) {
-                console.log(error);
-            }
-            
-            })
-            router.route('/:id').delete((req,res,next)=> {
-                try {
-                    
-                } catch (error) {
-                    console.log(error);
-                }
-                
-                })
-            
+		
+		res.send(products);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.route('/').get(async (req, res, next) => {
+	try {
+		const products = await readDB(productsPath);
+		res.send(products);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.route('/').post(async (req, res, next) => {
+	try {
+		const products = await readDB(productsPath);
+		const newProduct = {
+			...req.body,
+			_id: uniqid(),
+		};
+		products.push(newProduct);
+		await writeDB(productsPath, products);
+		res.send(newProduct);
+		console.log(req.body);
+	} catch (error) {
+		console.log(error);
+	}
+});
+router.route('/:id').get(async (req, res, next) => {
+	try {
+		const products = await readDB(productsPath);
+		const productByID = products.find((product) => req.params.id === product._id);
+		res.send(productByID);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.route('/:id').put(async (req, res, next) => {
+	try {
+		const products = await readDB(productsPath);
+		const update = products.map((product) =>
+			req.params.id === product._id ? { ...product, ...req.body } : product
+		);
+		await writeDB(productsPath, update);
+		res.send('ok');
+	} catch (error) {
+		console.log(error);
+	}
+});
+router.route('/:id').delete((req, res, next) => {
+	try {
+	} catch (error) {
+		console.log(error);
+	}
+});
 
 module.exports = router;
